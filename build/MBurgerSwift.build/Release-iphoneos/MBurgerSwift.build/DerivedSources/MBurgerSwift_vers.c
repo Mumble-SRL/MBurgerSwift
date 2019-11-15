56d07e8efc6204fc5be46442baf1f8f3c2f44e8c
lorenzOliveto <lorenzo.oliveto@gmail.com>
2019-11-15 12:12:05 +0100

    .gitignore is now working

--- build/MBurgerSwift.build/Release-iphoneos/MBurgerSwift.build/DerivedSources/MBurgerSwift_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char MBurgerSwiftVersionString[];
- extern const double MBurgerSwiftVersionNumber;
-
- const unsigned char MBurgerSwiftVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:MBurgerSwift  PROJECT:MBurgerSwift-1" "\n";
- const double MBurgerSwiftVersionNumber __attribute__ ((used)) = (double)1.;
