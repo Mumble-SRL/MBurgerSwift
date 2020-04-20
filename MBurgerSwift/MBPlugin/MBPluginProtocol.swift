6493a92468d1363b0520d1b9975c2dc6ce0360cb
lorenzOliveto <lorenzo.oliveto@gmail.com>
2020-04-20 16:51:52 +0200

    Added default values to plugin protocol

--- MBurgerSwift/MBPlugin/MBPluginProtocol.swift
+++ MBurgerSwift/MBPlugin/MBPluginProtocol.swift
@@ -9,12 +9,25 @@
 import Foundation
 
 /// A plugin that can be attached to add more functionalities to MBurger.
-public protocol MBPluginProtocol: Codable {
+public protocol MBPluginProtocol {
     
     /// The key used in the user dictionary, used to retrieve and set the data.
-    var userKey: String { get }
+    var userKey: String? { get }
     
     /// Returns a object that will be inserted in the pluginsObjects property of the user.
     /// - Parameter response: The response to read data from.
     func object(forUserResponse response: [String: Any]) -> Any?
+}
+
+/// Default values for the MBPluginProtocol
+public extension MBPluginProtocol {
+    /// Default value is nil -> no objectt is added to the user profile
+    var userKey: String? {
+        return nil
+    }
+    
+    /// Default value is nil -> no objectt is added to the user profile
+    func object(forUserResponse response: [String: Any]) -> Any? {
+        return nil
+    }
 }
