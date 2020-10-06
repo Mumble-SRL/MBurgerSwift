e6acdcd835ff05839a91613605a50edcf5f5fcd1
lorenzOliveto <lorenzo.oliveto@gmail.com>
2020-10-06 09:20:34 +0200

    Added parameters and updated doc

---
+++ MBurgerSwift/MBParameter/MBForceLocaleFallbackParameter.swift
@@ -0,0 +1,17 @@
+//
+//  MBForceLocaleParameter.swift
+//  MBurgerSwift
+//
+//  Created by Lorenzo Oliveto on 06/10/2020.
+//  Copyright © 2020 Mumble S.r.l (https://mumbleideas.it/). All rights reserved.
+//
+
+import Foundation
+
+/// A parameter to force the locale fallback, if an element is empty the fallback will be returned instead
+public struct MBForceLocaleFallbackParameter: MBParameter {
+    /// Returns the `Parameters` rapresentation of the object.
+    public func parameterRepresentation() -> [String: Any] {
+        return ["force_locale_fallback": "true"]
+    }
+}
---
+++ MBurgerSwift/MBParameter/MBOriginalMediaParameter.swift
@@ -0,0 +1,17 @@
+//
+//  MBOriginalMediaParameter.swift
+//  MBurgerSwift
+//
+//  Created by Lorenzo Oliveto on 06/10/2020.
+//  Copyright © 2020 Mumble S.r.l (https://mumbleideas.it/). All rights reserved.
+//
+
+import UIKit
+
+/// A parameter to request original version of the images
+public struct MBOriginalMediaParameter: MBParameter {
+    /// Returns the `Parameters` rapresentation of the object.
+    public func parameterRepresentation() -> [String: Any] {
+        return ["original_media": "true"]
+    }
+}
